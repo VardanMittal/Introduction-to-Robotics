577df8491bf112fd6d384f424081557b282fcad4
VardanMittal <vardanmittal000@gmail.com>
2024-08-04 15:07:12 +0530

    some urdf added and changes comitted

---
+++ Assignment 1/roswk/src/franka_simulation/launch/sim.launch.py
@@ -0,0 +1,54 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+import xacro
+
+def generate_launch_description():
+
+
+    declare_use_sim_time = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='false',
+        description='Use simulation (Gazebo) clock if true'
+    )
+
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    # Process the URDF file with xacro
+    pkg_path = get_package_share_directory('franka_simulation')
+    xacro_file = os.path.join(pkg_path, 'urdf', 'open_manipulator.urdf.xacro')
+    doc = xacro.process_file(xacro_file)
+    robot_description = {'robot_description': doc.toxml()}
+
+    # Create the robot_state_publisher node
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[robot_description, {'use_sim_time': use_sim_time}]
+    )
+
+    node_joint_state_publisher = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        output='screen'
+    )
+
+    rviz_config_path = "/root/surveillance-swarm/code/ros2_wk/src/hexapod/config/hexapod_rviz_config.rviz"
+    node_rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', rviz_config_path]
+    )
+
+    return LaunchDescription([
+        declare_use_sim_time,
+        node_robot_state_publisher,
+        node_joint_state_publisher,
+        node_rviz
+    ])
--- Assignment 1/roswk/src/franka_simulation/setup.py
+++ Assignment 1/roswk/src/franka_simulation/setup.py
@@ -1,5 +1,6 @@
 from setuptools import find_packages, setup
-
+import os
+import glob
 package_name = 'franka_simulation'
 
 setup(
@@ -10,6 +11,11 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+         (os.path.join('share', package_name, 'description'), glob('description/*.xacro')),
+        (os.path.join('share', package_name, 'meshes'),
+         glob('meshes/*.STL')),
+        (os.path.join('share', package_name, 'launch'),
+         glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
