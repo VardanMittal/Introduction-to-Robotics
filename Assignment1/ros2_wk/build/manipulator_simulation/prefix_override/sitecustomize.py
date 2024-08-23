6124040f5bd9a1296b0a636a6e6c888c2d84628d
VardanMittal <vardanmittal000@gmail.com>
2024-08-23 19:24:18 +0530

    controller simultion visit

--- Assignment1/ros2_wk/build/manipulator_simulation/prefix_override/sitecustomize.py
+++ Assignment1/ros2_wk/build/manipulator_simulation/prefix_override/sitecustomize.py
@@ -1,3 +1,4 @@
 import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/vardan/Introduction-to-Robotics/Assignment1/ros2_wk/install/manipulator_simulation'
+if sys.prefix == '/usr':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = '/home/vardan/Introduction-to-Robotics/Assignment1/ros2_wk/install/manipulator_simulation'
