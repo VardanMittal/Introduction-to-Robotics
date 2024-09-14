eae9a281efe527e16ca5345fe15be1f4b23f82e0
VardanMittal <vardanmittal000@gmail.com>
2024-09-14 14:42:16 +0530

    Project deleted

--- Module4/OWL_food/build/owl_sim/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/vardan/Introduction-to-Robotics/Module4/OWL_food/install/owl_sim'
--- Module4/OWL_food/build/owl_sim/setup.py
+++
@@ -1,1 +0,0 @@
-/home/vardan/Introduction-to-Robotics/Module4/OWL_food/src/owl_sim/setup.py
